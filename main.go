266a202f399e11b98abd24c6f628d7f9aaee50af
Bosco Vallejo-Nágera Esteban <juan.vallejonagera@newryglobal.com>
2023-12-05 10:04:51 +0100

    Part two done

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func main() {
 	sum := 0
 	var prevPositions []numPos
 	prevSymbols := make([]bool, 1000)
+	var prevMap map[int][]int
 
 	for scanner.Scan() {
 		text := scanner.Text()
@@ -31,10 +32,10 @@ func main() {
 		}
 		text = "." + text + "."
 		symbols := make([]bool, len(text))
+		curMap := make(map[int][]int)
 		var positions []numPos
 		checkingNum := false
 		currentPos := 0
-	posLoop:
 		for i, v := range text {
 			if unicode.IsDigit(v) {
 				if !checkingNum {
@@ -54,17 +55,14 @@ func main() {
 						continue
 					}
 					if symbols[i] {
-						sum += value
-						continue
+						curMap[i] = append(curMap[i], value)
 					}
 					if symbols[currentPos-1] {
-						sum += value
-						continue
+						curMap[currentPos-1] = append(curMap[currentPos-1], value)
 					}
 					for j := currentPos - 1; j <= end+1; j++ {
 						if prevSymbols[j] {
-							sum += value
-							continue posLoop
+							prevMap[j] = append(prevMap[j], value)
 						}
 					}
 					positions = append(positions, numPos{begin: currentPos, end: end, value: value})
@@ -73,17 +71,26 @@ func main() {
 			}
 
 		}
-	prevLoop:
 		for _, pos := range prevPositions {
 			for i := pos.begin - 1; i <= pos.end+1; i++ {
 				if symbols[i] {
-					sum += pos.value
-					continue prevLoop
+					curMap[i] = append(curMap[i], pos.value)
 				}
+			}
+		}
+		for _, l := range prevMap {
+			if len(l) == 2 {
+				sum += l[0] * l[1]
 			}
 		}
 		prevSymbols = symbols
 		prevPositions = positions
+		prevMap = curMap
+	}
+	for _, l := range prevMap {
+		if len(l) == 2 {
+			sum += l[0] * l[1]
+		}
 	}
 
 	fmt.Println(sum)
